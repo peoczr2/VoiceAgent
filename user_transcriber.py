01b405ebd98a3f6077a98a99a6c466e67a15066c
peoczr <peoczr@t-online.hu>
2025-07-13 22:00:05 +0200

    Okey

--- user_transcriber.py
+++
@@ -1,7 +0,0 @@
-from events import AudioEvent, TranscriptEvent
-from typing import AsyncIterator
-
-class UserTranscriber:
-    async def stream(self, media_event: AudioEvent) -> AsyncIterator[TranscriptEvent]:
-        # Placeholder for actual transcription logic
-        yield TranscriptEvent(text="...user speech...")
